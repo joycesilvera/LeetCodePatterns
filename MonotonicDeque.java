1f138adb7392621e6c8ec7e10bd75d050c0d4185
Joyce Silvera <60859729+joycesilvera@users.noreply.github.com>
2025-04-30 00:00:10 -0700

    add ext

--- MonotonicDeque.java
+++ MonotonicDeque.java
@@ -38,23 +38,23 @@ public class MonotonicDeque {
     }
 
     public static void main(String[] args) {
-        int[] test1 = {8, 2, 4, 7};
+        int[] test1 = { 8, 2, 4, 7 };
         int limit1 = 4;
         System.out.println("Test 1: " + longestSubarray(test1, limit1)); // Expected: 2
 
-        int[] test2 = {10, 1, 2, 4, 7, 2};
+        int[] test2 = { 10, 1, 2, 4, 7, 2 };
         int limit2 = 5;
         System.out.println("Test 2: " + longestSubarray(test2, limit2)); // Expected: 4
 
-        int[] test3 = {4, 2, 2, 2, 4, 4, 2, 2};
+        int[] test3 = { 4, 2, 2, 2, 4, 4, 2, 2 };
         int limit3 = 0;
         System.out.println("Test 3: " + longestSubarray(test3, limit3)); // Expected: 3
 
-        int[] test4 = {1, 5, 6, 7, 8, 10, 6, 5, 6};
+        int[] test4 = { 1, 5, 6, 7, 8, 10, 6, 5, 6 };
         int limit4 = 4;
         System.out.println("Test 4: " + longestSubarray(test4, limit4)); // Expected: 5
 
-        int[] test5 = {1, 2, 3, 4, 5};
+        int[] test5 = { 1, 2, 3, 4, 5 };
         int limit5 = 10;
         System.out.println("Test 5: " + longestSubarray(test5, limit5)); // Expected: 5
     }
